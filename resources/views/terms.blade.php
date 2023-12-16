f03070d8784638619899fc552526ec28dcd2eab6
Samiul Islam <samiulislamsharan@gmail.com>
2023-12-17 00:20:21 +0600

    create terms blade template

---
+++ resources/views/terms.blade.php
@@ -0,0 +1,15 @@
+@extends('layout.layout')
+
+@section('content')
+    <h1>Terms</h1>
+    <p>
+    <ul>
+        <li>Don't be a jerk</li>
+        <li>Don't spam</li>
+        <li>Don't post inappropriate content</li>
+        <li>Don't post illegal content</li>
+        <li> Pornography is not allowed</li>
+        </div>
+    </ul>
+    </p>
+@endsection
