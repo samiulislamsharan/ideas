4be1beaecb04d2f5976e2739cb2c88a68c3d6e33
Samiul Islam <samiulislamsharan@gmail.com>
2023-12-17 00:18:58 +0600

    make dashboard controller

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -8,6 +8,25 @@ class DashboardController extends Controller
 {
     public function index()
     {
-        return view('welcome');
+        $users = [
+            [
+                'name' => 'John Doe',
+                'age' => 25,
+            ],
+            [
+                'name' => 'Jane Doe',
+                'age' => 26,
+            ],
+            [
+                'name' => 'Johnny Doe',
+                'age' => 17,
+            ]
+        ];
+        return view(
+            'dashboard',
+            [
+                'users' => $users
+            ]
+        );
     }
 }
