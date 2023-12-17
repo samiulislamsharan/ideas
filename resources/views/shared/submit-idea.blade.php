8ab77eb0d199cd7ea570dc11427d02844120d7ab
Samiul Islam <samiulislamsharan@gmail.com>
2023-12-18 02:50:22 +0600

    update idea blade template
    updated idea blade template to show form validation error

--- resources/views/shared/submit-idea.blade.php
+++ resources/views/shared/submit-idea.blade.php
@@ -4,6 +4,10 @@
         @csrf
         <div class="mb-3">
             <textarea name="idea" class="form-control" id="idea" rows="3"></textarea>
+            {{-- display error if form validation fails --}}
+            @error('idea')
+                <div class="d-block mt-2 fs-6 text-danger"> {{ $message }} </div>
+            @enderror
         </div>
         <div class="">
             <button type="submit" class="btn btn-dark"> Share </button>
