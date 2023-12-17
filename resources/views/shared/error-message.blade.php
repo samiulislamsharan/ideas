720e4eb66f8d432a5036141c394f1b518f73e610
Samiul Islam <samiulislamsharan@gmail.com>
2023-12-18 02:49:38 +0600

    update messages
    added control flow to show status messages

--- resources/views/shared/error-message.blade.php
+++ resources/views/shared/error-message.blade.php
@@ -1,4 +1,6 @@
-<div class="alert alert-danger alert-dismissible fade show" role="alert">
-    Idea created Successfully
-    <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
-</div>
+@if (session()->has('errorMessage'))
+    <div class="alert alert-danger alert-dismissible fade show" role="alert">
+        Idea created Successfully
+        <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
+    </div>
+@endif
--- resources/views/shared/success-message.blade.php
+++ resources/views/shared/success-message.blade.php
@@ -1,4 +1,6 @@
-<div class="alert alert-success alert-dismissible fade show" role="alert">
-    Idea created Successfully
-    <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
-</div>
+@if (session()->has('successMessage'))
+    <div class="alert alert-success alert-dismissible fade show" role="alert">
+        {{ session('successMessage') }}}
+        <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
+    </div>
+@endif
