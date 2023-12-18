7fff5270f0ede1c52dbe743e88a7d3e7a2eed1cf
Samiul Islam <samiulislamsharan@gmail.com>
2023-12-19 02:28:23 +0600

    create search bartemplate
    search box is now a separate blade template

---
+++ resources/views/shared/search-bar.blade.php
@@ -0,0 +1,10 @@
+<div class="card">
+    <div class="card-header pb-0 border-0">
+        <h5 class="">Search</h5>
+    </div>
+    <div class="card-body">
+        <input placeholder="...
+            " class="form-control w-100" type="text" id="search">
+        <button class="btn btn-dark mt-2"> Search</button>
+    </div>
+</div>
