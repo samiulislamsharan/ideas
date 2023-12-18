6cd3254c046a6d01824b759f069ed5d7950525df
Samiul Islam <samiulislamsharan@gmail.com>
2023-12-19 02:25:26 +0600

    update Idea Controller
    idea controller now supports show, create and update

--- app/Http/Controllers/IdeaController.php
+++ app/Http/Controllers/IdeaController.php
@@ -7,27 +7,47 @@ use Illuminate\Http\Request;
 
 class IdeaController extends Controller
 {
+    public function store()
+    {
+        request()->validate(
+            [
+                'content' => 'required|min:5|max:255',
+            ]
+        );
+
+        $idea = Idea::create(
+            [
+                'content' => request()->get('content', '')
+            ]
+        );
+
+        return redirect()->route('dashboard')->with('successMessage', 'Idea created successfully!');
+    }
 
     public function show(Idea $idea)
     {
         return view('ideas.show', compact('idea'));
     }
 
-    public function store()
+    public function edit(Idea $idea)
+    {
+        $editing = true;
+
+        return view('ideas.show', compact('idea', 'editing'));
+    }
+
+    public function update(Idea $idea)
     {
         request()->validate(
             [
-                'idea' => 'required|min:5|max:255',
+                'content' => 'required|min:5|max:255',
             ]
         );
 
-        $idea = Idea::create(
-            [
-                'content' => request()->get('idea', '')
-            ]
-        );
+        $idea->content = request()->get('content', '');
+        $idea->save();
 
-        return redirect()->route('dashboard')->with('successMessage', 'Idea created successfully!');
+        return redirect()->route('idea.show', $idea->id)->with('successMessage', 'Idea updated successfully!');
     }
 
     public function destroy(Idea $idea)
