0ce427348d0df9f06d8a67275a23df7925ba5487
Samiul Islam <samiulislamsharan@gmail.com>
2023-12-18 23:03:47 +0600

    update web routes

--- routes/web.php
+++ routes/web.php
@@ -17,9 +17,15 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', [DashboardController::class, 'index']) ->name('dashboard');
+Route::get('/', [DashboardController::class, 'index'])->name('dashboard');
 
-    Route::post('/idea', [IdeaController::class, 'store'])->name('idea.create');
+Route::post('/idea', [IdeaController::class, 'store'])->name('idea.create');
+
+Route::get('/idea/{idea}', [IdeaController::class, 'show'])->name('idea.show');
+
+Route::get('/idea/{idea}/edit', [IdeaController::class, 'edit'])->name('idea.edit');
+
+Route::delete('/ideas/{id}', [IdeaController::class, 'destroy'])->name('ideas.destroy');
 
 Route::get('/profile', [ProfileController::class, 'index']);
 
