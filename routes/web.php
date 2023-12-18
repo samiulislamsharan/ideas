afd55bc94f8126c7b26b9e1040342c79c4b12a42
Samiul Islam <samiulislamsharan@gmail.com>
2023-12-19 02:30:13 +0600

    update web routes
    feat:
    add update route
    
    refactor:
    post delete route

--- routes/web.php
+++ routes/web.php
@@ -25,7 +25,9 @@ Route::get('/idea/{idea}', [IdeaController::class, 'show'])->name('idea.show');
 
 Route::get('/idea/{idea}/edit', [IdeaController::class, 'edit'])->name('idea.edit');
 
-Route::delete('/ideas/{id}', [IdeaController::class, 'destroy'])->name('ideas.destroy');
+Route::put('/idea/{idea}', [IdeaController::class, 'update'])->name('idea.update');
+
+Route::delete('/ideas/{idea}', [IdeaController::class, 'destroy'])->name('idea.destroy');
 
 Route::get('/profile', [ProfileController::class, 'index']);
 
