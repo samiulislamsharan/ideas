1fca19de02d1bbc4d503c65e8e65fa367e09e8ad
Samiul Islam <samiulislamsharan@gmail.com>
2023-12-21 15:22:19 +0600

    refactor search-bar blade template
    implemented search functionality

--- resources/views/shared/search-bar.blade.php
+++ resources/views/shared/search-bar.blade.php
@@ -3,8 +3,10 @@
         <h5 class="">Search</h5>
     </div>
     <div class="card-body">
-        <input placeholder="...
-            " class="form-control w-100" type="text" id="search">
-        <button class="btn btn-dark mt-2"> Search</button>
+        <form action="{{ route('dashboard') }}" method="GET">
+            <input name="search" placeholder="search
+            " class="form-control w-100" type="text">
+            <button class="btn btn-dark mt-2">Search</button>
+        </form>
     </div>
 </div>
