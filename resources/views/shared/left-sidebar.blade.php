4a3ee3ceab0c4ceb35e35a6753abec983e4240a5
Samiul Islam <samiulislamsharan@gmail.com>
2023-12-21 15:21:32 +0600

    refactor left-sidebar blade template
    sidebar links are now directed to their respected routes

--- resources/views/shared/left-sidebar.blade.php
+++ resources/views/shared/left-sidebar.blade.php
@@ -2,7 +2,7 @@
     <div class="card-body pt-3">
         <ul class="nav nav-link-secondary flex-column fw-bold gap-2">
             <li class="nav-item">
-                <a class="nav-link text-dark" href="#">
+                <a class="nav-link text-dark" href="{{ route('dashboard') }}">
                     <span>Home</span></a>
             </li>
             <li class="nav-item">
@@ -10,11 +10,11 @@
                     <span>Explore</span></a>
             </li>
             <li class="nav-item">
-                <a class="nav-link" href="#">
+                <a class="nav-link" href="{{ route('feed') }}">
                     <span>Feed</span></a>
             </li>
             <li class="nav-item">
-                <a class="nav-link" href="#">
+                <a class="nav-link" href="{{ route('terms') }}">
                     <span>Terms</span></a>
             </li>
             <li class="nav-item">
