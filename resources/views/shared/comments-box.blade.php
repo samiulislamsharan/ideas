1be6cdf76e1db7635bb199c97ecdef8c3736b732
Samiul Islam <samiulislamsharan@gmail.com>
2023-12-22 16:15:43 +0600

    update comment box blade template
    comment box blade template now can render data from model

--- resources/views/shared/comments-box.blade.php
+++ resources/views/shared/comments-box.blade.php
@@ -1,32 +1,36 @@
 <div>
     {{-- comment input --}}
-    <div class="mb-3">
-        <textarea class="fs-6 form-control" rows="1"></textarea>
-    </div>
-    <div>
-        <button class="btn btn-primary btn-sm"> Post Comment </button>
-    </div>
+    <form action="{{ route('ideas.comments.store', $idea->id) }}" method="POST">
+        @csrf
+        <div class="mb-3">
+            <textarea name="content" class="fs-6 form-control" rows="1"></textarea>
+            {{-- display error if form validation fails --}}
+            @error('content')
+                <div class="d-block mt-2 fs-6 text-danger"> {{ $message }} </div>
+            @enderror
+        </div>
+        <div>
+            <button type="submit" class="btn btn-primary btn-sm"> Post Comment </button>
+        </div>
+    </form>
 
     <hr>
-    {{-- comments --}}
-    <div class="d-flex align-items-start">
-        <img style="width:35px" class="me-2 avatar-sm rounded-circle"
-            src="https://api.dicebear.com/6.x/fun-emoji/svg?seed=Luigi" alt="Luigi Avatar">
-        <div class="w-100">
-            <div class="d-flex justify-content-between">
-                <h6 class="">Luigi
-                </h6>
-                <small class="fs-6 fw-light text-muted"> 3 hour
-                    ago</small>
+    {{-- all comments --}}
+    @foreach ($idea->comments as $comment)
+        <div class="d-flex align-items-start">
+            <img style="width:35px" class="me-2 avatar-sm rounded-circle"
+                src="https://api.dicebear.com/6.x/fun-emoji/svg?seed=Luigi" alt="Luigi Avatar">
+            <div class="w-100">
+                <div class="d-flex justify-content-between">
+                    <h6 class="">Luigi</h6>
+                    <small class="fs-6 fw-light text-muted">
+                        {{ $comment->created_at }}
+                    </small>
+                </div>
+                <p class="fs-6 mt-3 fw-light">
+                    {{ $comment->content }}
+                </p>
             </div>
-            <p class="fs-6 mt-3 fw-light">
-                and 1.10.33 of "de Finibus Bonorum et Malorum" (The Extremes of Good and
-                Evil)
-                by
-                Cicero, written in 45 BC. This book is a treatise on the theory of ethics,
-                very
-                popular during the Renaissan
-            </p>
         </div>
-    </div>
+    @endforeach
 </div>
