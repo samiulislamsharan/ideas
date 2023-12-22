2c9365d1eb3c222bf34107f37517621817e80892
Samiul Islam <samiulislamsharan@gmail.com>
2023-12-22 16:09:13 +0600

    update comment controler
    comment controller now able to validate and store data from view to the model

--- app/Http/Controllers/CommentController.php
+++ app/Http/Controllers/CommentController.php
@@ -2,9 +2,25 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Comment;
+use App\Models\Idea;
 use Illuminate\Http\Request;
 
 class CommentController extends Controller
 {
-    //
+    public function store(Idea $idea)
+    {
+        request()->validate(
+            [
+                'content' => 'required|min:1|max:255',
+            ]
+        );
+
+        $comment = new Comment();
+        $comment->idea_id = $idea->id;
+        $comment->content = request()->get('content', '');
+        $comment->save();
+
+        return redirect()->route('idea.show', $idea->id)->with('successMessage', 'Comment posted successfully!');
+    }
 }
