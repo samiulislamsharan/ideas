dd0564005b98a23c3e2c62a0d26fcee416c9da53
Samiul Islam <samiulislamsharan@gmail.com>
2023-12-25 00:29:27 +0600

    update ideas migration

--- database/migrations/2023_12_16_183648_create_ideas_table.php
+++ database/migrations/2023_12_16_183648_create_ideas_table.php
@@ -14,6 +14,7 @@ return new class extends Migration
         Schema::create('ideas', function (Blueprint $table) {
             $table->id();
             $table->string('content');
+            $table->foreignId('idea_id')->constrained()->cascadeOnDelete();
             $table->unsignedInteger('likes')->default(0);
             $table->timestamps();
         });
