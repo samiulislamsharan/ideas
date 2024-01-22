b9f023d219646253aa161766cdeefa2fdab3b13c
Samiul Islam <samiulislamsharan@gmail.com>
2024-01-22 15:08:02 +0600

    feat: add user_id foreign key to ideas table

--- database/migrations/2023_12_16_183648_create_ideas_table.php
+++ database/migrations/2023_12_16_183648_create_ideas_table.php
@@ -13,8 +13,8 @@ return new class extends Migration
     {
         Schema::create('ideas', function (Blueprint $table) {
             $table->id();
+            $table->foreignId('user_id')->constrained()->cascadeOnDelete();
             $table->string('content');
-            $table->foreignId('idea_id')->constrained()->cascadeOnDelete();
             $table->unsignedInteger('likes')->default(0);
             $table->timestamps();
         });
