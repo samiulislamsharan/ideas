71d1443a8d888a9e3f4895cd45690ab68a9093f7
Samiul Islam <samiulislamsharan@gmail.com>
2024-01-22 15:26:30 +0600

    feat: show ideas if user is logged in, otherwise tell user to log in

--- resources/views/shared/submit-idea.blade.php
+++ resources/views/shared/submit-idea.blade.php
@@ -1,16 +1,22 @@
-<h4> Share yours ideas </h4>
-<div class="row">
-    <form action="{{ route('idea.create') }}" method="post">
-        @csrf
-        <div class="mb-3">
-            <textarea name="content" class="form-control" id="content" rows="3" placeholder="Any idea today?"></textarea>
-            {{-- display error if form validation fails --}}
-            @error('content')
-                <div class="d-block mt-2 fs-6 text-danger"> {{ $message }} </div>
-            @enderror
-        </div>
-        <div class="">
-            <button type="submit" class="btn btn-dark"> Share </button>
-        </div>
-    </form>
-</div>
+@auth()
+    <h4> Share yours ideas </h4>
+    <div class="row">
+        <form action="{{ route('idea.create') }}" method="post">
+            @csrf
+            <div class="mb-3">
+                <textarea name="content" class="form-control" id="content" rows="3" placeholder="Any idea today?"></textarea>
+                {{-- display error if form validation fails --}}
+                @error('content')
+                    <div class="d-block mt-2 fs-6 text-danger"> {{ $message }} </div>
+                @enderror
+            </div>
+            <div class="">
+                <button type="submit" class="btn btn-dark"> Share </button>
+            </div>
+        </form>
+    </div>
+@endauth
+
+@guest()
+    <h4>Log in to Share yours ideas </h4>
+@endguest
