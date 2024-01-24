ba26a2fa7678fb4264186d32c195a7973177edee
Samiul Islam <samiulislamsharan@gmail.com>
2024-01-24 20:24:13 +0600

    feat: add user show blade template to render user profile

---
+++ resources/views/users/show.blade.php
@@ -0,0 +1,34 @@
+@extends('layout.layout')
+
+@section('content')
+    <div class="row">
+        <div class="col-3">
+            @include('shared.left-sidebar')
+        </div>
+        <div class="col-6">
+            @include('shared.success-message')
+            <div class="mt-3">
+                @include('shared.user-card')
+            </div>
+            <hr>
+
+            @forelse ($ideas as $idea)
+                <div class="mt-3">
+                    @include('shared.idea-card')
+                </div>
+            @empty
+                <div class="alert alert-info text-center">
+                    No ideas found.
+                </div>
+            @endforelse
+            <div class="mt-3">
+                {{-- pagination --}}
+                {{ $ideas->withQueryString()->links() }}
+            </div>
+        </div>
+        <div class="col-3">
+            @include('shared.search-bar')
+            @include('shared.follow-box')
+        </div>
+    </div>
+@endsection
