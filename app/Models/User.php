95625db1d037809ee366e3d0ffae3926d722dd59
Samiul Islam <samiulislamsharan@gmail.com>
2024-01-24 19:30:31 +0600

    feat: UserModel now has one-to-many relationship with CommentModel

--- app/Models/User.php
+++ app/Models/User.php
@@ -45,6 +45,10 @@ class User extends Authenticatable
     ];
 
     public function ideas(){
-        return $this->hasMany(Idea::class);
+        return $this->hasMany(Idea::class)->latest();
+    }
+
+    public function comments(){
+        return $this->hasMany(Comment::class);
     }
 }
