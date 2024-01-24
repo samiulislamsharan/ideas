66eb368be197a7811ebbc8e5fd62a55cd71dff55
Samiul Islam <samiulislamsharan@gmail.com>
2024-01-24 19:28:45 +0600

    refactor: search bar attributes to retain search value in the search box

--- resources/views/shared/search-bar.blade.php
+++ resources/views/shared/search-bar.blade.php
@@ -4,8 +4,8 @@
     </div>
     <div class="card-body">
         <form action="{{ route('dashboard') }}" method="GET">
-            <input name="search" placeholder="search
-            " class="form-control w-100" type="text">
+            <input value="{{ request('search', '') }}" name="search" placeholder="search" class="form-control w-100"
+                type="text">
             <button class="btn btn-dark mt-2">Search</button>
         </form>
     </div>
