04b716edb53f7ee45390e28100673823451125f5
Samiul Islam <samiulislamsharan@gmail.com>
2024-01-24 19:24:06 +0600

    feat: add resource route for UserController

--- routes/web.php
+++ routes/web.php
@@ -5,7 +5,7 @@ use App\Http\Controllers\DashboardController;
 use App\Http\Controllers\FeedController;
 use App\Http\Controllers\IdeaController;
 use App\Http\Controllers\CommentController;
-use App\Http\Controllers\ProfileController;
+use App\Http\Controllers\UserController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -22,19 +22,15 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', [DashboardController::class, 'index'])->name('dashboard');
 
 
-Route::resource('ideas', IdeaController::class)
-    ->except(['index', 'create'])
-    ->middleware('auth');
+Route::resource('ideas', IdeaController::class)->except(['index', 'create'])->middleware('auth');
 
 Route::resource('ideas', IdeaController::class)->only(['show']);
 
 
-Route::resource('ideas.comments', CommentController::class)
-    ->only(['store'])
-    ->middleware('auth');
+Route::resource('ideas.comments', CommentController::class)->only(['store'])->middleware('auth');
 
 
-Route::get('/profile', [ProfileController::class, 'index'])->name('profile');
+Route::resource('users', UserController::class)->only(['show', 'edit', 'update'])->middleware('auth');
 
 Route::get('/feed', [FeedController::class, 'index'])->name('feed');
 
