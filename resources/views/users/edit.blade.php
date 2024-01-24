b3bc690dee615c68ad0d0bcf7ca1cfa60569adc5
Samiul Islam <samiulislamsharan@gmail.com>
2024-01-24 20:37:49 +0600

    feat: add user edit blade template to edit user info

---
+++ resources/views/users/edit.blade.php
@@ -0,0 +1,20 @@
+@extends('layout.layout')
+
+@section('content')
+    <div class="row">
+        <div class="col-3">
+            @include('shared.left-sidebar')
+        </div>
+        <div class="col-6">
+            @include('shared.success-message')
+            <hr>
+            <div class="mt-3">
+                @include('shared.user-card')
+            </div>
+        </div>
+        <div class="col-3">
+            @include('shared.search-bar')
+            @include('shared.follow-box')
+        </div>
+    </div>
+@endsection
