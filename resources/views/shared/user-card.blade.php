9f8eb75d56e1dbb1b749a4e60e66cb69bbfaa9b5
Samiul Islam <samiulislamsharan@gmail.com>
2024-01-28 17:18:32 +0600

    refactor: user profile now fetches profile image and bio from User Model

--- resources/views/shared/user-card.blade.php
+++ resources/views/shared/user-card.blade.php
@@ -2,15 +2,11 @@
     <div class="px-3 pt-4 pb-2">
         <div class="d-flex align-items-center justify-content-between">
             <div class="d-flex align-items-center">
-                <img style="width:150px" class="me-3 avatar-sm rounded-circle"
-                    src="https://api.dicebear.com/6.x/fun-emoji/svg?seed={{ $user->name }}" alt="{{ $user->name }}">
+                <img style="width:150px" class="me-3 avatar-sm rounded-circle" src="{{ $user->getImageURL() }}"
+                    alt="{{ $user->name }}">
                 <div>
-                    @if ($editing ?? false)
-                        <input value="{{ $user->name }}" type="text" class="form-control">
-                    @else
-                        <h3 class="card-title mb-0"><a href="#"> {{ $user->name }} </a></h3>
-                        <span class="fs-6 text-muted">{{ $user->email }}</span>
-                    @endif
+                    <h3 class="card-title mb-0"><a href="#"> {{ $user->name }} </a></h3>
+                    <span class="fs-6 text-muted">{{ $user->email }}</span>
                 </div>
             </div>
             <div>
@@ -23,24 +19,7 @@
         </div>
         <div class="px-2 mt-4">
             <h5 class="fs-5"> Bio : </h5>
-            @if ($editing ?? false)
-                <div class="mb-3">
-                    <textarea name="bio" class="form-control" id="bio" rows="3"></textarea>
-                    {{-- display error if form validation fails --}}
-                    @error('bio')
-                        <div class="d-block mt-2 fs-6 text-danger"> {{ $message }} </div>
-                    @enderror
-                </div>
-                <div class="">
-                    <button type="submit" class="btn btn-dark"> Save </button>
-                </div>
-            @else
-                <p class="fs-6 fw-light">
-                    This book is a treatise on the theory of ethics, very popular during the
-                    Renaissance. The first line of Lorem Ipsum, "Lorem ipsum dolor sit amet..", comes
-                    from a line in section 1.10.32.
-                </p>
-            @endif
+            <p class="fs-6 fw-light"> {{ $user->bio }} </p>
             <div class="mt-3 d-flex justify-content-start">
                 <a href="#" class="fw-light nav-link fs-6 me-3"> <span class="fas fa-user me-1">
                     </span> 120 Followers </a>
