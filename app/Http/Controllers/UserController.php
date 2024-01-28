66a9c5520122e15a30145959ff80d8a6ec015889
Samiul Islam <samiulislamsharan@gmail.com>
2024-01-28 17:29:43 +0600

    feat: add pagination, profile update validations, image uploading and being able to visit profile

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use App\Models\User;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Storage;
 
 class UserController extends Controller
 {
@@ -23,7 +24,9 @@ class UserController extends Controller
     public function edit(User $user)
     {
         $editing = true;
-        return view('users.edit', compact('user', 'editing'));
+        $ideas = $user->ideas()->paginate(5);
+
+        return view('users.edit', compact('user', 'editing', 'ideas'));
     }
 
     /**
@@ -31,10 +34,29 @@ class UserController extends Controller
      */
     public function update(User $user)
     {
-        //
+        $validated = request()->validate(
+            [
+                'name' => 'required|min:3|max:40',
+                'bio' => 'nullable|min:1|max:255',
+                'image' => 'image'
+            ]
+        );
+
+        if (request()->has('image')) {
+            $imagePath = request()->file('image')->store('profile', 'public');
+            $validated['image'] = $imagePath;
+
+            // Delete old image
+            Storage::disk('public')->delete($user->image ?? '');
+        }
+
+        $user->update($validated);
+
+        return redirect()->route('profile');
     }
 
-    /**
-     * Remove the specified resource from storage.
-     */
+    public function profile()
+    {
+        return $this->show(auth()->user());
+    }
 }
