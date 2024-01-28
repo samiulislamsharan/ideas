8e6e040b6dd5635774e5ea368365a7e713d7a999
Samiul Islam <samiulislamsharan@gmail.com>
2024-01-28 17:24:48 +0600

    refactor: show profile photo on comments

--- resources/views/shared/comments-box.blade.php
+++ resources/views/shared/comments-box.blade.php
@@ -18,8 +18,8 @@
     {{-- all comments --}}
     @foreach ($idea->comments as $comment)
         <div class="d-flex align-items-start">
-            <img style="width:35px" class="me-2 avatar-sm rounded-circle"
-                src="https://api.dicebear.com/6.x/fun-emoji/svg?seed={{ $comment->user->name }}" alt="{{ $comment->user->name }}">
+            <img style="width:35px" class="me-2 avatar-sm rounded-circle" src="{{ $comment->user->getImageURL() }}"
+                alt="{{ $comment->user->name }}">
             <div class="w-100">
                 <div class="d-flex justify-content-between">
                     <h6 class="">{{ $comment->user->name }}</h6>
