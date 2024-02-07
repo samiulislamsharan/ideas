40ab5ffc23baa6eaa29de9bdc778c85bcf530643
Samiul Islam <samiulislamsharan@gmail.com>
2024-02-07 12:16:46 +0600

    feat(view): Add user stats to user profile page

---
+++ resources/views/users/shared/user-stats.blade.php
@@ -0,0 +1,21 @@
+<div class="mt-3 d-flex justify-content-start">
+    <a href="#" class="fw-light nav-link fs-6 me-3">
+        <span class="fas fa-user me-1"> </span>
+        {{ $user->followers()->count() }}
+    </a>
+    <a href="#" class="fw-light nav-link fs-6 me-3">
+        <span class="fas fa-brain me-1">
+        </span>
+        {{ $user->ideas()->count() }}
+    </a>
+    <a href="#" class="fw-light nav-link fs-6 me-3">
+        <span class="fas fa-comment me-1">
+        </span>
+        {{ $user->comments()->count() }}
+    </a>
+    <a href="#" class="fw-light nav-link fs-6">
+        <span class="fas fa-heart me-1">
+        </span>
+        {{ $user->likes()->count() }}
+    </a>
+</div>
