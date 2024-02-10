35422ef467265ff4fd786bc462c30e84c1b08c01
Samiul Islam <samiulislamsharan@gmail.com>
2024-02-11 00:25:44 +0600

    feat(controller): add search functionality to FeedController

--- app/Http/Controllers/FeedController.php
+++ app/Http/Controllers/FeedController.php
@@ -2,12 +2,29 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Idea;
 use Illuminate\Http\Request;
 
 class FeedController extends Controller
 {
-    public function index()
+    /**
+     * Handle the incoming request.
+     */
+    public function __invoke(Request $request)
     {
-        return view('feed');
+        $followingIDs = auth()->user()->followings()->pluck('id');
+
+        $ideas = Idea::whereIn('user_id', $followingIDs)->latest();
+
+        if (request()->has('search')) {
+            $ideas = $ideas->where('content', 'like', '%' . request()->get('search', '') . '%');
+        }
+
+        return view(
+            'dashboard',
+            [
+                'ideas' => $ideas->paginate(5)
+            ]
+        );
     }
 }
