9e9550de754a9b2eeb4c4fb69e0a98af686b37c1
Samiul Islam <samiulislamsharan@gmail.com>
2024-02-11 23:13:36 +0600

    refactor(route): Refactor authentication routes to use middleware

--- routes/auth.php
+++ routes/auth.php
@@ -3,13 +3,11 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\AuthController;
 
-Route::get('/register', [AuthController::class, 'register'])->name('register');
+Route::group(['middleware' => 'guest'], function () {
+    Route::get('/register', [AuthController::class, 'register'])->name('register');
+    Route::post('/register', [AuthController::class, 'store']);
+    Route::get('/login', [AuthController::class, 'login'])->name('login');
+    Route::post('/login', [AuthController::class, 'authenticate']);
+});
 
-Route::post('/register', [AuthController::class, 'store']);
-
-
-Route::get('/login', [AuthController::class, 'login'])->name('login');
-
-Route::post('/login', [AuthController::class, 'authenticate']);
-
-Route::post('/logout', [AuthController::class, 'logout'])->name('logout');
+Route::post('/logout', [AuthController::class, 'logout'])->middleware('auth')->name('logout');
