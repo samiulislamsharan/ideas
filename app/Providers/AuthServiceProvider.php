b9f6fe645d4daa025609cf01631f25350ae2f55d
Samiul Islam <samiulislamsharan@gmail.com>
2024-02-11 20:20:49 +0600

    feat(provider): Add role and permission checks to AuthServiceProvider

--- app/Providers/AuthServiceProvider.php
+++ app/Providers/AuthServiceProvider.php
@@ -2,7 +2,9 @@
 
 namespace App\Providers;
 
-// use Illuminate\Support\Facades\Gate;
+use App\Models\Idea;
+use App\Models\User;
+use Illuminate\Support\Facades\Gate;
 use Illuminate\Foundation\Support\Providers\AuthServiceProvider as ServiceProvider;
 
 class AuthServiceProvider extends ServiceProvider
@@ -21,6 +23,18 @@ class AuthServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        //
+        // Role
+        Gate::define('admin', function (User $user): bool {
+            return (bool) $user->is_admin;
+        });
+
+        // Permission
+        Gate::define('idea.delete', function (User $user, Idea $idea): bool {
+            return $user->is_admin || $user->id === $idea->user_id;
+        });
+
+        Gate::define('idea.edit', function (User $user, Idea $idea): bool {
+            return $user->is_admin || $user->id === $idea->user_id;
+        });
     }
 }
