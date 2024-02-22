8d54e38bf6495e2bb38d7524f5f10175273e62c7
Samiul Islam <samiulislamsharan@gmail.com>
2024-02-22 11:47:15 +0600

    refactor(model): Add withCount property to Idea model

--- app/Models/Idea.php
+++ app/Models/Idea.php
@@ -11,6 +11,8 @@ class Idea extends Model
 
     protected $with = ['user:id,name,image', 'comments.user:id,name,image'];
 
+    protected $withCount = ['likes'];
+
     protected $fillable = [
         'user_id',
         'content',
