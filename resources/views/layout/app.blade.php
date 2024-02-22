7bc4758043dbebda9dc16ddd78db2f5ccfb836e1
Samiul Islam <samiulislamsharan@gmail.com>
2024-02-22 16:55:46 +0600

    refactor(view): Update page title in app layout

--- resources/views/layout/app.blade.php
+++ resources/views/layout/app.blade.php
@@ -5,7 +5,7 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
 
-    <title>{{ config('app.name') }}</title>
+    <title> @yield('title') | {{ config('app.name') }}</title>
 
     <link href="https://bootswatch.com/5/sketchy/bootstrap.min.css" rel="stylesheet" crossorigin="anonymous">
 
