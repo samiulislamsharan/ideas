aec03b98231fc63a3ab13cd02dc229a0adde350d
Samiul Islam <samiulislamsharan@gmail.com>
2024-02-23 02:08:53 +0600

    refactor(route): Add language route

--- routes/web.php
+++ routes/web.php
@@ -22,6 +22,11 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+Route::get('lang/{locale}', function ($locale) {
+    session()->put('locale', $locale);
+    return redirect()->back();
+})->name('lang');
+
 Route::get('/', [DashboardController::class, 'index'])->name('dashboard');
 
 
