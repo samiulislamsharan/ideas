28c71b0f2613ccac829ff4e83dce22789d205e0d
Samiul Islam <samiulislamsharan@gmail.com>
2024-02-23 02:04:53 +0600

    feat(language): Add language files for pagination, authentication, ideas, and passwords

---
+++ lang/en/ideas.php
@@ -0,0 +1,25 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Ideas Language Lines
+    |--------------------------------------------------------------------------
+    |
+    | The following language lines are used during ideas for various
+    | messages that we need to display to the user. You are free to modify
+    | these language lines according to your application's requirements.
+    |
+    */
+
+    'en'=>'EN',
+    'bn'=>'BN',
+    'share_ideas' => 'Share yours ideas',
+    'log_into_share_ideas' => 'Log into Share Ideas',
+    'share' => 'Share',
+    'any_idea' => 'Any idea today?',
+    'search' => 'Search',
+    'search_here'=>'search',
+
+];
