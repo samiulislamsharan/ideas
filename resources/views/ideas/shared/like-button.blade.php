c2d4af3fd52d0aa4734c5d3cd3892db7ed886942
Samiul Islam <samiulislamsharan@gmail.com>
2024-02-22 11:47:29 +0600

    refactor(view): Update like button display to use likes_count instead of calling likes() method

--- resources/views/ideas/shared/like-button.blade.php
+++ resources/views/ideas/shared/like-button.blade.php
@@ -5,7 +5,7 @@
                 @csrf
                 <button type="submit" class="fw-light nav-link fs-6">
                     <span class="fas fa-heart me-1"> </span>
-                    {{ $idea->likes()->count() }}
+                    {{ $idea->likes_count }}
                 </button>
             </form>
         @else
@@ -13,7 +13,7 @@
                 @csrf
                 <button type="submit" class="fw-light nav-link fs-6">
                     <span class="far fa-heart me-1"> </span>
-                    {{ $idea->likes()->count() }}
+                    {{ $idea->likes_count }}
                 </button>
             </form>
         @endif
@@ -22,7 +22,7 @@
     @guest
         <a href="{{ route('login') }}" class="fw-light nav-link fs-6">
             <span class="far fa-heart me-1"> </span>
-            {{ $idea->likes()->count() }}
+            {{ $idea->likes_count }}
         </a>
     @endguest
 </div>
