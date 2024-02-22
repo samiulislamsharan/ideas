ba9a4af6a0eeb08cde501c9973e9cd3236531517
Samiul Islam <samiulislamsharan@gmail.com>
2024-02-23 02:05:06 +0600

    feat(language): Add Bengali language files for pagination, authentication, ideas, and passwords

---
+++ lang/bn/ideas.php
@@ -0,0 +1,25 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Ideas Language Lines
+    |--------------------------------------------------------------------------
+    |
+    | The following language lines are used during ideas for various
+    | messages that we need to display to the user. You are free to modify
+    | these language lines according to your application's requirements.
+    |
+    */
+
+    'en' => 'ইংরেজি',
+    'bn' => 'বাংলা',
+    'share_ideas' => 'আইডিয়া শেয়ার করুন',
+    'log_into_share_ideas' => 'আইডিয়া শেয়ার করার জন্য লগ ইন করুন',
+    'share' => 'শেয়ার করুন',
+    'any_idea' => 'আজ কোন আইডিয়া ?',
+    'search' => 'খুঁজছেন ?',
+    'search_here' => 'খুঁজুন',
+
+];
