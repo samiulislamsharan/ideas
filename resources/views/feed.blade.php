0deca0e13fb1e6f72ff0f733613a7eb74c2a10cc
Samiul Islam <samiulislamsharan@gmail.com>
2024-02-24 03:18:30 +0600

    refactor(view): Refactor feed view to use layout and include shared components

--- resources/views/feed.blade.php
+++ resources/views/feed.blade.php
@@ -1,1 +1,33 @@
-<h1> Feed </h1>
+@extends('layout.app')
+
+@section('title', 'Feed')
+
+@section('content')
+    <div class="row">
+        <div class="col-3">
+            @include('shared.left-sidebar')
+        </div>
+        <div class="col-6">
+            @include('shared.success-message')
+            @include('ideas.shared.submit-idea')
+            <hr>
+            @forelse ($ideas as $idea)
+                <div class="mt-3">
+                    @include('ideas.shared.idea-card')
+                </div>
+            @empty
+                <div class="alert alert-info text-center">
+                    No Ideas found.
+                </div>
+            @endforelse
+            <div class="mt-3">
+                {{-- pagination --}}
+                {{ $ideas->withQueryString()->links() }}
+            </div>
+        </div>
+        <div class="col-3">
+            @include('shared.search-bar')
+            @include('shared.follow-box')
+        </div>
+    </div>
+@endsection
