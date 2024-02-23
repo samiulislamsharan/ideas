b31484472c00f500ed7d9b4c86bc1ad7eb07d3ac
Samiul Islam <samiulislamsharan@gmail.com>
2024-02-23 23:35:20 +0600

    feat(ServiceProvider): Add topUsers variable to AppServiceProvider

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,8 +2,10 @@
 
 namespace App\Providers;
 
+use App\Models\User;
 use Illuminate\Contracts\Pagination\Paginator;
 use Illuminate\Pagination\Paginator as PaginationPaginator;
+use Illuminate\Support\Facades\View;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -22,5 +24,13 @@ class AppServiceProvider extends ServiceProvider
     public function boot(): void
     {
         PaginationPaginator::useBootstrapFive();
+
+        View::share(
+            'topUsers',
+            User::withCount('ideas')
+                ->orderBy('ideas_count', 'DESC')
+                ->limit(5)
+                ->get()
+        );
     }
 }
