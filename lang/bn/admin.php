c221619a4b5a1a3ba940337e1543a3165b69e42e
Samiul Islam <samiulislamsharan@gmail.com>
2024-02-25 09:55:33 +0600

    feat(language): Add admin language files for Bengali and English

---
+++ lang/bn/admin.php
@@ -0,0 +1,18 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Admin Language Lines
+    |--------------------------------------------------------------------------
+    |
+    | The following language lines are used during admin for various
+    | messages that we need to display to the user. You are free to modify
+    | these language lines according to your application's requirements.
+    |
+    */
+    'dashboard' => 'ড্যাশবোর্ড',
+    'admin_dashboard' => 'অ্যাডমিন ড্যাশবোর্ড',
+    'users' => 'ব্যবহারকারী',
+];
