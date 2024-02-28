688b978cc5d86fc2643a7b4dc0cd2b7227266eb7
Samiul Islam <samiulislamsharan@gmail.com>
2024-02-29 01:00:30 +0600

    feat(view): Add widgets to admin dashboard

--- resources/views/admin/dashboard.blade.php
+++ resources/views/admin/dashboard.blade.php
@@ -8,6 +8,28 @@
         <div class="col-9">
             <h1>{{ __('admin.admin_dashboard') }}</h1>
             @include('shared.success-message')
-        </div>
-    </div>
-@endsection
+
+            <div class="row">
+                <div class="col-sm-6 col-md-4">
+                    @include('admin.shared.widget', [
+                        'icon' => 'fas fa-users',
+                        'title' => __('admin.total_users'),
+                        'data' => $totalUsers,
+                    ])
+                </div>
+                <div class="col-sm-6 col-md-4">
+                    @include('admin.shared.widget', [
+                        'icon' => 'fas fa-brain',
+                        'title' => __('admin.total_ideas'),
+                        'data' => $totalIdeas,
+                    ])
+                </div>
+                <div class="col-sm-6 col-md-4">
+                    @include('admin.shared.widget', [
+                        'icon' => 'fas fa-comments',
+                        'title' => __('admin.total_comments'),
+                        'data' => $totalComments,
+                    ])
+                </div>
+            </div>
+        @endsection
