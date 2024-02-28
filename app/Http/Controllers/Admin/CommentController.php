4a7986c6d1c68087b20f1612f2af835c641bed05
Samiul Islam <samiulislamsharan@gmail.com>
2024-02-28 16:54:31 +0600

    feat(controller): Add CommentController to handle comments in the admin panel

---
+++ app/Http/Controllers/Admin/CommentController.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Http\Controllers\Admin;
+
+use App\Http\Controllers\Controller;
+use App\Models\Comment;
+use Illuminate\Http\Request;
+
+class CommentController extends Controller
+{
+    public function index()
+    {
+        $comments = Comment::latest()->paginate(10);
+
+        return view('admin.comments.index', compact('comments'));
+    }
+
+    public function destroy(Comment $comment)
+    {
+        $comment->delete();
+
+        return back()->with('success', 'Comment was deleted successfully!');
+    }
+}
