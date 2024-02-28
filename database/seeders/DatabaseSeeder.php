4f02a78814448d0e98713079e296bb1e0890ffbf
Samiul Islam <samiulislamsharan@gmail.com>
2024-02-29 00:27:58 +0600

    feat(seeder): Add seeders for Idea and Comment models

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -12,7 +12,9 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        // \App\Models\User::factory(10)->create();
+        \App\Models\User::factory(10)->create();
+        \App\Models\Idea::factory(20)->create();
+        \App\Models\Comment::factory(50)->create();
 
         // \App\Models\User::factory()->create([
         //     'name' => 'Test User',
