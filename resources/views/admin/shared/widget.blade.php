c6eff70ab30e6956cf4a6ee3757109c6ee13bcf4
Samiul Islam <samiulislamsharan@gmail.com>
2024-02-29 01:00:55 +0600

    feat(view): Add widget.blade.php file

---
+++ resources/views/admin/shared/widget.blade.php
@@ -0,0 +1,6 @@
+<div class="card">
+    <div class="card-body">
+        <h2 class="card-title"> <span class="{{ $icon ?? 'fas fa-info' }}"></span> {{ $title ?? '' }} </h2>
+        <p class="fs-4 card-text"> {{ $data ?? 0 }} </p>
+    </div>
+</div>
