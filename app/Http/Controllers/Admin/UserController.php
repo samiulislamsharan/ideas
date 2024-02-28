be42039da15387648407f3b6d85f3bb56b748683
Samiul Islam <samiulislamsharan@gmail.com>
2024-02-28 16:55:43 +0600

    refactor(controller): Update pagination limit in UserController

--- app/Http/Controllers/Admin/UserController.php
+++ app/Http/Controllers/Admin/UserController.php
@@ -5,13 +5,12 @@ namespace App\Http\Controllers\Admin;
 use App\Http\Controllers\Controller;
 use App\Models\User;
 use Illuminate\Http\Request;
-use Illuminate\Support\Facades\Log;
 
 class UserController extends Controller
 {
     public function index()
     {
-        $users = User::latest()->paginate(5);
+        $users = User::latest()->paginate(10);
 
         return view('admin.users.index', compact('users'));
     }
