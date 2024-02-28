1fbcb94a836c238db091c126fb217b7350eb7542
Samiul Islam <samiulislamsharan@gmail.com>
2024-02-28 16:56:19 +0600

    feat(view): Add admin views for comments and ideas

---
+++ resources/views/admin/comments/index.blade.php
@@ -0,0 +1,61 @@
+@extends('layout.app')
+
+@section('title', 'Ideas Admin - Comments')
+
+@section('content')
+    <div class="row">
+        <div class="col-3">
+            @include('admin.shared.left-sidebar')
+        </div>
+        <div class="col-9">
+            <h1>{{ __('admin.comments') }}</h1>
+            @include('shared.success-message')
+
+            <table class="table table-striped table-hover">
+                <thead class="table-dark">
+                    <th>ID</th>
+                    <th>User</th>
+                    <th>Idea</th>
+                    <th>Content</th>
+                    <th>Created At</th>
+                    <th>Actions</th>
+                </thead>
+
+                <tbody>
+                    @foreach ($comments as $comment)
+                        <tr>
+                            <td>{{ $comment->id }}</td>
+                            <td>
+                                <a href="{{ route('users.show', $comment->user) }}">
+                                    {{ $comment->user->name }}
+                                </a>
+                            </td>
+                            <td>
+                                <a href="{{ route('ideas.show', $comment->idea) }}">
+                                    {{ $comment->idea->id }}
+                                </a>
+                            </td>
+                            <td>{{ $comment->content }}</td>
+                            <td>{{ $comment->created_at->toDateString() }}</td>
+                            <td>
+                                {{-- <a href="{{ route('comment.edit', $comment) }}" class="btn btn-primary btn-sm">Edit</a>
+                                <a href="{{ route('comment.show', $comment) }}" class="btn btn-primary btn-sm">View</a> --}}
+                                <form action="{{ route('admin.comments.destroy', $comment) }}", method="POST">
+                                    @csrf
+                                    @method('DELETE')
+                                    <a href="#!" class="btn btn-danger btn-sm"
+                                        onclick="this.closest('form').submit(); return false;">
+                                        Delete
+                                    </a>
+                                </form>
+                            </td>
+                        </tr>
+                    @endforeach
+                </tbody>
+            </table>
+
+            {{ $comments->links() }}
+
+        </div>
+    </div>
+@endsection
---
+++ resources/views/admin/ideas/index.blade.php
@@ -0,0 +1,48 @@
+@extends('layout.app')
+
+@section('title', 'Ideas Admin - Ideas')
+
+@section('content')
+    <div class="row">
+        <div class="col-3">
+            @include('admin.shared.left-sidebar')
+        </div>
+        <div class="col-9">
+            <h1>{{ __('admin.ideas') }}</h1>
+            @include('shared.success-message')
+
+            <table class="table table-striped table-hover">
+                <thead class="table-dark">
+                    <th>ID</th>
+                    <th>User</th>
+                    <th>Content</th>
+                    <th>Created At</th>
+                    <th>Actions</th>
+                </thead>
+
+                <tbody>
+                    @foreach ($ideas as $idea)
+                        <tr>
+                            <td>{{ $idea->id }}</td>
+                            <td>
+                                <a href="{{ route('users.show', $idea->user) }}">
+                                    {{ $idea->user->name }}
+                                </a>
+                            </td>
+                            <td>{{ $idea->content }}</td>
+                            <td>{{ $idea->created_at->toDateString() }}</td>
+                            <td>
+                                <a href="{{ route('ideas.edit', $idea) }}" class="btn btn-primary btn-sm">Edit</a>
+                                <a href="{{ route('ideas.show', $idea) }}" class="btn btn-primary btn-sm">View</a>
+                                <a href="#!" class="btn btn-danger btn-sm">Delete</a>
+                            </td>
+                        </tr>
+                    @endforeach
+                </tbody>
+            </table>
+
+            {{ $ideas->links() }}
+
+        </div>
+    </div>
+@endsection
