41896fe5623557e51ee27152a0159c22fbfe26a2
Samiul Islam <samiulislamsharan@gmail.com>
2024-03-15 22:35:37 +0600

    refactor(view): Update column classes in user edit view

--- resources/views/users/edit.blade.php
+++ resources/views/users/edit.blade.php
@@ -2,10 +2,10 @@
 
 @section('content')
     <div class="row">
-        <div class="col-3">
+        <div class="col-lg-3 col-sm-12 mb-3">
             @include('shared.left-sidebar')
         </div>
-        <div class="col-6">
+        <div class="col-lg-6 col-sm-12">
             @include('shared.success-message')
             <div class="mt-3">
                 @include('users.shared.user-edit-card')
@@ -26,7 +26,7 @@
                 {{ $ideas->withQueryString()->links() }}
             </div>
         </div>
-        <div class="col-3">
+        <div class="col-lg-3 col-sm-12">
             @include('shared.search-bar')
             @include('shared.follow-box')
         </div>
