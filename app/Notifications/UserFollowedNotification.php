6edb5b97f9c18514c5f32b5d98015ff4312195be
Samiul Islam <samiulislamsharan@gmail.com>
2024-04-25 15:18:58 +0600

    Add UserFollowedNotification class for sending notifications when a user is followed

---
+++ app/Notifications/UserFollowedNotification.php
@@ -0,0 +1,62 @@
+<?php
+
+namespace App\Notifications;
+
+use App\Models\User;
+use Illuminate\Bus\Queueable;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Notifications\Messages\MailMessage;
+use Illuminate\Notifications\Notification;
+
+class UserFollowedNotification extends Notification
+{
+    use Queueable;
+
+    private $follower;
+
+    /**
+     * Create a new notification instance.
+     *
+     * @return void
+     */
+    public function __construct(User $follower)
+    {
+        $this->follower = $follower;
+    }
+
+    /**
+     * Get the notification's delivery channels.
+     *
+     * @param  mixed  $notifiable
+     * @return array
+     */
+    public function via($notifiable)
+    {
+        return ['database']; // or you can use 'mail' if you want to send via email
+    }
+
+    /**
+     * Get the array representation of the notification.
+     *
+     * @param  mixed  $notifiable
+     * @return array
+     */
+    public function toArray($notifiable)
+    {
+        return [
+            'follower_id' => $this->follower->id,
+            'follower_name' => $this->follower->name,
+        ];
+    }
+
+    // mark as read
+    public function markAsRead($id)
+    {
+        if($id)
+        {
+            auth()->user()->notifications()->where('id', $id)->first()->markAsRead();
+        }
+
+        return back();
+    }
+}
