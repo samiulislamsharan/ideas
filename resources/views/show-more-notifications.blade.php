f02ec6c102d668d27bb7c3f7f215e38f92e11096
Samiul Islam <samiulislamsharan@gmail.com>
2024-04-25 15:17:00 +0600

    Add show-more-notifications view for displaying additional notifications

---
+++ resources/views/show-more-notifications.blade.php
@@ -0,0 +1,19 @@
+@extends('layout.app')
+
+@section('title', 'Notifications')
+
+@section('content')
+    <div class="row">
+        <div class="col-lg-3 col-sm-12">
+            @include('shared.left-sidebar')
+        </div>
+        <div class="col-lg-6 col-sm-12">
+            <h1>{{ __('ideas.notifications') }}</h1>
+            @include('shared.notification-card')
+        </div>
+        <div class="col-lg-3 col-sm-12">
+            @include('shared.search-bar')
+            @include('shared.follow-box')
+        </div>
+    </div>
+@endsection
