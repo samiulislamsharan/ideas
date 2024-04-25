00c47691f97e6abea106f6dce56da01464c0f099
Samiul Islam <samiulislamsharan@gmail.com>
2024-04-25 15:18:52 +0600

    Add notification feature to FollowerController

--- app/Http/Controllers/FollowerController.php
+++ app/Http/Controllers/FollowerController.php
@@ -4,23 +4,30 @@ namespace App\Http\Controllers;
 
 use App\Models\User;
 use Illuminate\Http\Request;
+use App\Notifications\UserFollowedNotification;
 
 class FollowerController extends Controller
 {
     public function follow(User $user)
     {
+        // follow the user, create a notification and return the notification card and redirect back
         $follower = auth()->user();
 
         $follower->followings()->attach($user);
+
+        $user->notify(new UserFollowedNotification($follower));
 
         return redirect()->route('users.show', $user->id)->with('success', "Followed successfully!");
     }
 
     public function unfollow(User $user)
     {
+        // unfollow the user, delete the notification and redirect back
         $follower = auth()->user();
 
         $follower->followings()->detach($user);
+
+        $user->notifications()->where('type', UserFollowedNotification::class)->where('data->follower_id', $follower->id)->delete();
 
         return redirect()->route('users.show', $user->id)->with('success', "Unfollowed successfully!");
     }
